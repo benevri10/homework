31e4efa5a952b6ee02b638f05d211725c1d3fd5d
ben evri <benevri5@gmail.com>
2022-07-04 22:09:24 +0300

    first commit

---
+++ homeWork.js
@@ -0,0 +1,73 @@
+console.log({
+  name: "hackerU",
+  courses: {
+    1: "קורס סייבר והבטחת מידע",
+    2: "קורס בדיקת תוכנה",
+    3: "קורס גרפיקה ועיצוב דיגיטלי",
+    4: "קורס ניהול רשתות ואבטחת מידע",
+    5: "קורס פיתוח תוכנה",
+    6: "קורס אנימציה",
+    7: "קורס פיתוח אוטומציה",
+    8: "קורס גיימינג ופיתוח משחקים",
+    9: "קורס ניהול מוצר",
+    10: "קורס בניית אתרים",
+  },
+  fullStackDevelopmentCourse: {
+    classes: {
+      w12345678: "night",
+      w040422mr:"morning",
+    },
+    w040422mrClass: {
+      lecturer: "daniel zadok",
+      houseKeeper: null,
+      students: {
+        1: "asyar",
+        2: "dani matis",
+        3: "hanna mzeget",
+        4: "nadav ohel",
+        5: "niv regev",
+        6: "or katayev",
+        7: "re'em",
+        8: "roi gal",
+        9: "ruby jakov",
+        10: "ari weisberg",
+        11: "aviv keinen",
+        12: "christina hen",
+        13: "daniel brailovsky",
+        14: "dantaf",
+        15: "dor shaked",
+        16: "eli yakov",
+        17: "hadar mil",
+        18: "inbal",
+        19: "maayanski",
+        20: "maor wer",
+        21: "mari chin",
+        22: "matan ifrach",
+        23: "maximum",
+        24: "michael ben",
+        25: "mikush",
+        26: "ofir shaya",
+        27: "ohad lan",
+        28: "omer bar",
+        29: "orel cohen",
+        30: "raziel morad",
+        31: "reefgb",
+        32: "sagi",
+        33: "tom rossner",
+        34: "david zuk",
+        35:"ben evri",
+      },
+      benEvri: {
+        name: "ben",
+        lastName: "evri",
+        age: 21,
+        married: false,
+        address: {
+          city: "holon",
+          street:"hertzfeld 11"
+        }
+      }
+    }
+  }
+
+});
